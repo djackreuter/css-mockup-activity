b4f99d4d4c59dffb17ec74e500d6f1e75569129f
Dylan Reuter <djreuter45@gmail.com>
2017-07-18 16:34:30 -0600

    added css to section 2

--- index.php
+++ index.php
@@ -46,19 +46,21 @@
 
 				<!-- SECTION ONE -->
 				<section>
-					<div class="container">
-						<img class="pull-left" src="images/stunning.jpg" alt="captain picard" />
-						<h2>A Day in the Life...</h2>
-						<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
-						<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
-						<div class="clearfix"></div>
+					<div class="container1">
+						<div class="content">
+							<img class="pull-left" src="images/stunning.jpg" alt="captain picard" />
+							<h2>A Day in the Life...</h2>
+							<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
+							<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
+							<div class="clearfix"></div>
+						</div>
 					</div>
 				</section>
 
 				<!-- SECTION TWO -->
 				<section>
 					<div class="container">
-						<h2>Summary of Profe$$ional Skillz</h2>
+						<h2 id="table-heading">Summary of Profe$$ional Skillz</h2>
 						<table>
 							<tr>
 								<th>Languages</th>
